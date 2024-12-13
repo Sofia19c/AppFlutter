c7416c17ca88e6fe93a2516d87c5a298aacc4c3c
sofia19c <sofivampaireknight@gmail.com>
2024-12-13 10:50:53 -0500

    elimine una importacion que tenia repetida

--- flutter_application_1/lib/pages/empezarAhora.dart
+++ flutter_application_1/lib/pages/empezarAhora.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/registrate.dart';
 import 'package:flutter_application_1/pages/iniciarSesion.dart';
 
